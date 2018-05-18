910582a0771bfd79f1028199211214888252c2a4
ViterbiDevelopment <wangchaonihao@163.com>
2018-05-18 21:17:49 +0800

    待完善

---
+++ friendClass/friendClass/Tv.cpp
@@ -0,0 +1,8 @@
+//
+//  Tv.cpp
+//  friendClass
+//
+//  Created by Viterbi on 2018/5/18.
+//  Copyright © 2018年 Viterbi. All rights reserved.
+//
+
---
+++ friendClass/friendClass/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  friendClass
+//
+//  Created by Viterbi on 2018/5/18.
+//  Copyright © 2018年 Viterbi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
